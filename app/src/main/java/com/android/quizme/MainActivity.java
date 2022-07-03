d3e3fc3347652e8980d80481ec66abb8c6f54d83
Ranjeet Singh <ranjeetsingh98965@gmail.com>
2022-07-03 18:45:16 +0700

    initial commit

---
+++ app/src/main/java/com/android/quizme/MainActivity.java
@@ -0,0 +1,71 @@
+package com.android.quizme;
+
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.android.quizme.databinding.ActivityMainBinding;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, new HomeFragment());
+        transaction.commit();
+
+        binding.bottonBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                switch (i) {
+                    case 0:
+                        transaction.replace(R.id.content, new HomeFragment());
+                        transaction.commit();
+                        break;
+                    case 1:
+                        transaction.replace(R.id.content, new LeaderboardsFragment());
+                        transaction.commit();
+                        break;
+                    case 2:
+                        transaction.replace(R.id.content, new WalletFragment());
+                        transaction.commit();
+                        break;
+                    case 3:
+                        transaction.replace(R.id.content, new ProfileFragment());
+                        transaction.commit();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if(item.getItemId() == R.id.wallet) {
+//            Toast.makeText(this,"Wallet is clicked.", Toast.LENGTH_SHORT).show();
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/com/android/quizme/SpinWheel/model/LuckyItem.java
@@ -0,0 +1,12 @@
+package com.android.quizme.SpinWheel.model;
+
+
+
+public class LuckyItem {
+    public String topText;
+    public String secondaryText;
+    public int secondaryTextOrientation;
+    public int icon;
+    public int color;
+    public int textColor;
+}
